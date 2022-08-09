6dd8864f855ec1131fdc963699aba17821816f13
Nolan Gormley <nolangormley@gmail.com>
2022-08-08 22:21:41 -0400

    geo admin page is live, all current data is geocoded

--- website/TransSafetyMap/map/admin.py
+++ website/TransSafetyMap/map/admin.py
@@ -1,5 +1,10 @@
+from django.contrib.gis import admin as geo_admin
 from django.contrib import admin
 from .models import Incident
 
 # Register your models here.
-admin.site.register(Incident)
+class IncidentAdmin(geo_admin.ModelAdmin):
+    readonly_fields = ('id',)
+    # list_display = ('year',  'severity', 'city', 'state')
+
+geo_admin.site.register(Incident, IncidentAdmin)
--- website/TransSafetyMap/map/models.py
+++ website/TransSafetyMap/map/models.py
@@ -1,13 +1,16 @@
-from django.db import models
+# from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
 
 class Incident(models.Model):
-    id       = models.UUIDField(null=False, primary_key=True)
-    city     = models.CharField(max_length=128, blank=True, null=True)
-    state    = models.CharField(max_length=2, blank=True, null=True)
-    year     = models.IntegerField(blank=True, null=True)
-    severity = models.CharField(max_length=128, blank=True, null=True)
+    id              = models.UUIDField(null=False, primary_key=True)
+    city            = models.CharField(max_length=128, blank=True, null=True)
+    state           = models.CharField(max_length=2, blank=True, null=True)
+    year            = models.IntegerField(blank=True, null=True)
+    severity        = models.CharField(max_length=128, blank=True, null=True)
+    geocode_output  = models.CharField(max_length=128, blank=True, null=True)
+    geocoded_point  = models.PointField(srid=4326)
 
     class Meta:
         managed = False
