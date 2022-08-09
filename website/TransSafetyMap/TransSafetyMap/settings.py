690f5ecf35565d86308da2b33ae5d84c3dccf65f
Nolan Gormley <nolangormley@gmail.com>
2022-08-08 22:33:41 -0400

    added a leaflet map

--- website/TransSafetyMap/TransSafetyMap/settings.py
+++ website/TransSafetyMap/TransSafetyMap/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-    'map.apps.MapConfig'
+    'map.apps.MapConfig',
+    'leaflet',
 ]
 
 MIDDLEWARE = [
--- website/TransSafetyMap/map/admin.py
+++ website/TransSafetyMap/map/admin.py
@@ -1,9 +1,10 @@
 from django.contrib.gis import admin as geo_admin
+from leaflet.admin import LeafletGeoAdminMixin
 from django.contrib import admin
 from .models import Incident
 
 # Register your models here.
-class IncidentAdmin(geo_admin.ModelAdmin):
+class IncidentAdmin(LeafletGeoAdminMixin, geo_admin.ModelAdmin):
     readonly_fields = ('id',)
     # list_display = ('year',  'severity', 'city', 'state')
 
